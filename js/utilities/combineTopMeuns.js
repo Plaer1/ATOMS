9a174495d00bceed3b7ef8c2fe79dbd854a80a14
Plaer1 <bryant.eliott@gmail.com>
2024-11-13 01:06:54 -0500

    removed unnessicarry wait calls

--- js/utilities/combineTopMeuns.js
+++ js/utilities/combineTopMeuns.js
@@ -6,14 +6,8 @@ import waitForElement from './waitForElement.js';
 
 // Waits for the target element to be present before moving elements
 export default function combineTopMenus() {
-bigWait();
-bigWait();
   waitForElement('._39oUCO1OuizVPwcnnv88no', function(element) {
-	bigWait();
-	bigWait();
 	waitForElement('._3Z3ohQ8-1NKnCZkbS6fvy', function(element) {
-		bigWait();
-		bigWait();
 		waitForElement('._3Z3ohQ8-1NKnCZkbS6fvy', function(element) {
 
 			const targetClass = '._39oUCO1OuizVPwcnnv88no'; // the top bar

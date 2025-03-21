1d5b7f5cf78f522ee172b933a2bae798bbca0cb5
Plaer1 <bryant.eliott@gmail.com>
2025-03-20 20:42:04 -0400

    Fixed to be less hard-coded

--- js/utilities/rootMenus/modifySteamRootMenu.js
+++ js/utilities/rootMenus/modifySteamRootMenu.js
@@ -1,31 +1,40 @@
-import addSeperator from "./addSeperator.js";
 import addMenuItem from "./addMenuItem.js";
 import getTranslation from "../translate.js";
 import waitForElement from "../waitForElement.js";
 
 export default async function modifySteamRootMenu() {
-	try {
+    try {
         const reloadSteamMenu = await getTranslation("reloadSteam", document.documentElement.lang);
-		const restartSteam = await getTranslation("restartSteam", document.documentElement.lang);
-        
-		waitForElement('#popup_target > div > div > div:nth-child(11)', function(element) {
+        console.log('Translation for reloadSteam:', reloadSteamMenu);
 
-			// Select the exit button from the Steam root menu.
-			const exitButton = document.querySelector('#popup_target > div > div > div:nth-child(11)');
-			// Get the parent element of the exit button.
-			const parentElement = exitButton.parentNode;
+        const restartSteam = await getTranslation("restartSteam", document.documentElement.lang);
+        console.log('Translation for restartSteam:', restartSteam);
 
-			// Add "Reload Steam" menu item before the exit button.
-			addMenuItem(reloadSteamMenu, '#popup_target > div > div > div:nth-child(11)', 'before', "location.reload()");
-			// Add "Restart Steam" menu item before the exit button.
-			 addMenuItem(restartSteam, '#popup_target > div > div > div:nth-child(12)', 'before', "SteamClient.User.StartRestart(false)");
-			// Add a separator to the menu.
-			addMenuItem("", '#popup_target > div > div > div:nth-child(13)', 'before', "", ['_2jXHP0742MyApMUVUM8IFn', '_21GPYlKBCLsHQpTsHw_RL_']);
-		});
-		
+        waitForElement('._2EstNjFIIZm_WUSKm5Wt7n._3pofGqV0buiKAfMPEs3_82', function(menuContainer) {
+            console.log('Menu container found:', menuContainer);
+
+            const items = menuContainer.querySelectorAll('._2jXHP0742MyApMUVUM8IFn._21GPYlKBCLsHQpTsHw_RL_');
+            const lastItem = items[items.length - 1];
+            console.log('Last menu item:', lastItem);
+
+            if (lastItem) {
+                // Insert separator after the last menu item.
+                const separator = addMenuItem("", '._2EstNjFIIZm_WUSKm5Wt7n._3pofGqV0buiKAfMPEs3_82 > ._2jXHP0742MyApMUVUM8IFn._21GPYlKBCLsHQpTsHw_RL_:last-of-type', 'after', "", ['_2jXHP0742MyApMUVUM8IFn', '_21GPYlKBCLsHQpTsHw_RL_']);
+                console.log('Separator added:', separator);
+
+                // Insert "Reload Steam" menu item after separator.
+                const reloadItem = addMenuItem(reloadSteamMenu, '._2EstNjFIIZm_WUSKm5Wt7n._3pofGqV0buiKAfMPEs3_82 > ._2jXHP0742MyApMUVUM8IFn._21GPYlKBCLsHQpTsHw_RL_:last-of-type', 'after', "location.reload()");
+                console.log('Reload Steam menu item added:', reloadItem);
+
+                // Insert "Restart Steam" menu item after "Reload Steam".
+                const restartItem = addMenuItem(restartSteam, '._2EstNjFIIZm_WUSKm5Wt7n._3pofGqV0buiKAfMPEs3_82 > ._2jXHP0742MyApMUVUM8IFn._21GPYlKBCLsHQpTsHw_RL_:last-of-type', 'after', "SteamClient.User.StartRestart(false)");
+                console.log('Restart Steam menu item added:', restartItem);
+            } else {
+                console.warn('Could not find the last menu item to insert after.');
+            }
+        });
 
     } catch (error) {
         console.error('Error modifying Steam root menu:', error.message);
     }
-
 }
